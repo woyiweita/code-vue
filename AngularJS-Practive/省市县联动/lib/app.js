fb2e32854313779135d0b0ac163cb1029f28f996
Charles <woyiweita@sina.com>
2018-04-03 17:18:59 +0800

    Angularjs 练习

---
+++ AngularJS-Practive/省市县联动/lib/app.js
@@ -0,0 +1,10 @@
+/** 
+ *@class index 
+ *@description App主模块,所有的模块被主模块引用 
+ *@time 2014-09-20 10:53 
+ *@author StarZou 
+ **/  
+  
+  
+var App = angular.module('App', ['mgcrea.ngStrap', 'App.controllers']);  
+console.log(App+"daole"); 
